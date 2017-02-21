f035163d877c38cd1cce0b1ce55a6a2c563dcac9
Thuan Nguyen <thuannguyen@Thuans-MacBook-Pro.local>
2017-02-20 23:40:32 -0800

    Adding demo and readme file

---
+++ Yelp/ReviewTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReviewTableViewCell.swift
+//  Yelp
+//
+//  Created by Thuan Nguyen on 2/19/17.
+//  Copyright © 2017 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class ReviewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
